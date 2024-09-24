f8559e8b408609d1254faa4b2d9a45da25c940f8
unknown <ryli@nx.com>
2024-09-24 11:33:33 +0800

    commit

--- 13_原型和原型链/03_Function和Object的prototype.js
+++ 13_原型和原型链/03_Function和Object的prototype.js
@@ -1,13 +1,12 @@
-console.log(Function)
-console.log(Function.prototype)
-console.log(Object)
-console.log(Object.__proto__)
-console.log(Object.__proto__ === Function.prototype)
-console.log(Function.prototype.__proto__)
-console.log(Function.prototype.__proto__ === Object.prototype)
+console.log(Function);
+console.log(Function.prototype);
+console.log(Object);
+console.log(Object.__proto__);
+console.log(Object.__proto__ === Function.prototype);
+console.log(Function.prototype.__proto__);
+console.log(Function.prototype.__proto__ === Object.prototype);
 
-console.log(Function.__proto__, Function.__proto__ === Object.prototype)
-
+console.log(Function.__proto__, Function.__proto__ === Object.prototype);
 
 // function Function() {}
 /**
@@ -22,39 +21,37 @@ console.log(Function.__proto__, Function.__proto__ === Object.prototype)
  * Foo.prototype.__proto__ = Object.prototype
  * Function.prototype.__proto__ = Object.prototype
  */
-function Foo() {
+function Foo() {}
 
-}
-
-console.log(Object.getOwnPropertyDescriptor(Object, "prototype"))
-console.log(Object.prototype.__proto__)
-console.log(Object.__proto__ === Function.prototype)
-console.log(Function.prototype === Function.__proto__)
-console.log(Function.prototype.__proto__ === Object.prototype)
+console.log(Object.getOwnPropertyDescriptor(Object, "prototype"));
+console.log(Object.prototype.__proto__);
+console.log(Object.__proto__ === Function.prototype);
+console.log(Function.prototype === Function.__proto__);
+console.log(Function.prototype.__proto__ === Object.prototype);
 
 const obj = {
-  name: "lry"
-}
+  name: "lry",
+};
 
 const info = Object.create(obj, {
-  age: {
-    enumerable: true,
-    value: 18
-  }
-})
+  age: {
+    enumerable: true,
+    value: 18,
+  },
+});
 
-console.log(info.__proto__ === obj)
+console.log(info.__proto__ === obj);
 // isPrototypeOf 检查原型链上的对象
-console.log(obj.isPrototypeOf(info))
-console.log(obj.__proto__.isPrototypeOf(info))
+console.log(obj.isPrototypeOf(info));
+console.log(obj.__proto__.isPrototypeOf(info));
 
 function Foo() {}
-const foo = new Foo()
+const foo = new Foo();
 // foo -> Foo.prototype -> Object.prototype -> null
-console.log(foo.__proto__ === Foo.prototype)
-console.log(foo.__proto__.__proto__ === Object.prototype)
-console.log(foo.__proto__.__proto__.__proto__)
+console.log(foo.__proto__ === Foo.prototype);
+console.log(foo.__proto__.__proto__ === Object.prototype);
+console.log(foo.__proto__.__proto__.__proto__);
 // instanceof 检查原型链上的对象构造器
-console.log(foo instanceof Foo)
-console.log(foo instanceof Function)
-console.log(foo instanceof Object)
+console.log(foo instanceof Foo);
+console.log(foo instanceof Function);
+console.log(foo instanceof Object);
